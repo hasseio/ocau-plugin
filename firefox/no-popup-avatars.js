4d42fe5071714861cd357977bd0b5f275a8741e8
Hasse <me@hasse.io>
2018-02-22 09:40:10 +1000

    Add a couple more avatar suppression observers

--- firefox/no-popup-avatars.js
+++ firefox/no-popup-avatars.js
@@ -1,8 +1,8 @@
 /*
  * Remove avatars from showing on popups
  */
-__ocau_plugin.observers.noPopupAvatar = {
-    name: 'no-popup-avatar',
+__ocau_plugin.observers.noAlertsAvatar = {
+    name: 'no-alerts-avatar',
     state: 'hideAvatars',
     selector: '.alertsPopup',
     process: function(element) {
@@ -10,5 +10,26 @@ __ocau_plugin.observers.noPopupAvatar = {
         avatars.forEach(function(element){
             element.remove()
         });
+    }
+};
+
+__ocau_plugin.observers.noInboxAvatar = {
+    name: 'no-inbox-avatar',
+    state: 'hideAvatars',
+    selector: '.listPlaceholder div[class=""]:first-child',
+    process: function(element) {
+        var avatars = element.querySelectorAll('.avatar');
+        avatars.forEach(function(element){
+            element.remove()
+        });
+    }
+};
+
+__ocau_plugin.observers.noUserAvatar = {
+    name: 'no-user-avatar',
+    state: 'hideAvatars',
+    selector: '.menuHeader .avatar',
+    process: function(element) {
+        element.remove();
     }
 };
